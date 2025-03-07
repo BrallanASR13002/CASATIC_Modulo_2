0042baa9b1c1db7c5855dd81a24a70562a66ac62
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 11:21:58 -0600

    Add Excerside2 project files and implement basic console output

--- Guia2/Excerside1/Program.cs
+++ Guia2/Excerside1/Program.cs
@@ -7,24 +7,24 @@
         Print("Conteo del numero de palabras en una cadena");
         Print($"La cantidad de palabras en la cadena es: {CountWords(data)}");
     }
-     public static string DataInput()
-        {
-            string data;
-            Print("Ingrese la frase: ");
-            data = Console.ReadLine();
-            return data;
-        }
-        public static string Print(string message)
-        {
-            ConsoleColor color = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine(message);
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.ForegroundColor = color;
-            return message;
-        }
-          public static int CountWords(string data)
+    public static string DataInput()
+    {
+        string data;
+        Print("Ingrese la frase: ");
+        data = Console.ReadLine();
+        return data;
+    }
+    public static string Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+        return message;
+    }
+    public static int CountWords(string data)
     {
         if (string.IsNullOrWhiteSpace(data))
         {
