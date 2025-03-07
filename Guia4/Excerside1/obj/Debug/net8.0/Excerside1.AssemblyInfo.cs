aa0211ada98a04f0910c768f251e72ee8b0ac904
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 14:09:02 -0600

    Update assembly information and binaries for Excerside1

--- Guia4/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
+++ Guia4/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c499277b6e21e7e739569c6504fab9400e3fb509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440106855847b3d8976e6f49d40006af6b71226e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
