aa9ae08e642e423426f466624ca707da61bae673
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 10:08:44 -0600

    initial Excerside2

---
+++ Guia1/Excerside2/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+    public static int DataInput()
+        {
+            int number;
+            string data;
+            Print("Ingrese un numero: ");
+            data = Console.ReadLine();
+            while (!int.TryParse(data, out number))
+            {
+                Print("Ingrese un numero valido: ");
+                data = Console.ReadLine();
+            }
+            return number;
+        }
+        public static string Print(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.ForegroundColor = color;
+            return message;
+        }
+}
