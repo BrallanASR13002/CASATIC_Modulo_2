238f4f1620e6a3f54b153887be1bd053e5f9b963
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 15:10:38 -0600

    Implement DataInput method for user input and add Print method for formatted output

--- Guia6/Excerside1/Program.cs
+++ Guia6/Excerside1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+        public static int DataInput()
+    {
+        int number;
+        string data;
+        Print("Ingrese un numero: ");
+        data = Console.ReadLine();
+        while (!int.TryParse(data, out number))
+        {
+            Print("Ingrese un numero valido: ");
+            data = Console.ReadLine();
+        }
+        return number;
+    }
+    public static string Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+        return message;
+    }
+}
