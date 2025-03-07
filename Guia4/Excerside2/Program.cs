7cb65bf246e96244af88c2472dce5ee2cd146444
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 14:24:48 -0600

    Implement Program class with DataInput and Print methods for user interaction

--- Guia4/Excerside2/Program.cs
+++ Guia4/Excerside2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+    public static string DataInput(string prompt)
+    {
+        Print(prompt);
+        return Console.ReadLine();
+    }
+
+    public static string Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+        return message;
+    }
+}
