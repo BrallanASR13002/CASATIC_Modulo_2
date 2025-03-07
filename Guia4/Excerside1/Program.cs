3cb8163f2f95a3a89a3855e628c61a162c5ed532
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 13:44:55 -0600

    Add DataInput and Print methods for enhanced user interaction in Program.cs

--- Guia4/Excerside1/Program.cs
+++ Guia4/Excerside1/Program.cs
@@ -2,6 +2,23 @@
 {
     private static void Main(string[] args)
     {
-    
+
+
+    }
+    public static string DataInput(string prompt)
+    {
+        Print(prompt);
+        return Console.ReadLine();
+    }
+
+    public static string Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+        return message;
     }
 }
