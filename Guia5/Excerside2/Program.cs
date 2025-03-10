59020a910b764e2fbfe14ff7ed4995606abee5ff
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 06:31:10 -0600

    Implement basic console application structure and input validation for Excerside2

--- Guia5/Excerside2/Program.cs
+++ Guia5/Excerside2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Print("Ejemplo de pruebas unitarias en una operación matemática");
+        Print("División de dos numeros enteros");
+    }
+    public static int DataInput()
+    {
+        int number;
+        string data;
+        Print("Ingrese un numero: ");
+        data = Console.ReadLine();
+        while (!int.TryParse(data, out number) || number < 0)
+        {
+            Print("Ingrese un número válido (entero positivo o cero): ");
+            data = Console.ReadLine();
+        }
+        return number;
+    }
+    public static string Print(string message)
+    {   
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.ForegroundColor = color;
+        return message; 
+        Console.WriteLine("Presione Enter para continuar ...");
+        Console.ReadKey(); 
+        Console.Clear();
+    }
+}
