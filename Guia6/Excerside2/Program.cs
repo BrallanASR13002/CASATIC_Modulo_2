51cc671781e6753c2093d20ffa82539c6babcdf3
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 10:01:26 -0600

    Implement data input and print methods in Program class for user interaction

--- Guia6/Excerside2/Program.cs
+++ Guia6/Excerside2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+    public static double DataInput()
+    {
+        double number;
+        string data;
+        Print("Ingrese un numero: ");
+        data = Console.ReadLine();
+        while (!double.TryParse(data, out number) || number < 0)
+        {
+            Print("Ingrese un número válido (entero positivo o cero): ");
+            data = Console.ReadLine();
+        }
+        return number;
+    }
+    public static void Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("Presione Enter para continuar ...");
+        Console.ReadLine();
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+    }
+}
