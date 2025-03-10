e40c5907d73e96b2236adf1b8290e74db57520b6
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 12:01:08 -0600

    Add Program class with input handling and output methods

---
+++ Guia8/Exercise1/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Print("Documentation example");
+        //local vars
+        string input = Input();
+        int integerInput = Convert(input);
+        Print(integerInput.ToString());
+    }
+    public static string Input()
+    {
+
+        string data;
+        Print("Enter a data: ");
+        data = Console.ReadLine();
+        return data;
+    }
+    public static int Convert(string data)
+    {
+        int number;
+        while (true)
+        {
+            Print(data);
+            data = Console.ReadLine();
+            if (int.TryParse(data, out number))
+            {
+                return number;
+            }
+            else
+            {
+                Print("Invalid entry. Please enter a number.");
+            }
+        }
+    }
+
+    public static void Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+    }
+
+}
