ab23b59d61bce16218ca77056a18bba848d5bd59
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 08:39:58 -0600

    Refactor Division01 method in Exceside2Test to remove parameters and simplify division logic

--- Guia5/Excerside2.Tests/Exceside2Test.cs
+++ Guia5/Excerside2.Tests/Exceside2Test.cs
@@ -2,7 +2,7 @@ using Xunit;
 class Exceside2Test
 {
     [Fact]
-    public static double Division01(double 5, double 6)
+    public static double Division01()
     {
         double div = 5 / 6;
         return div;
