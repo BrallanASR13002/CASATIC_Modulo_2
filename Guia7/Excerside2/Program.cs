bf6906eb6abebd89fc828734962dd0fface18c12
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 11:32:07 -0600

    Implement input handling and sum calculation methods in Program class

--- Guia7/Excerside2/Program.cs
+++ Guia7/Excerside2/Program.cs
@@ -1,7 +1,32 @@
 ﻿internal class Program
 {
+    // static vars by the function calling count
+    private static int callCounter = 0;
     private static void Main(string[] args)
     {
+        Console.WriteLine("Input integer data:");
+        int integer1 = int.Parse(Console.ReadLine());
+        int integer2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Sum of integers: " + Sum(integer1, integer2));
+        Console.WriteLine("number of calling by Sum: " + callCounter);
 
+        Console.WriteLine("\nInput two numbers data:");
+        double real1 = double.Parse(Console.ReadLine());
+        double real2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Double's sum: " + Sum(real1, real2));
+        Console.WriteLine("number of calling by Sum: " + callCounter);
+    }
+    //override the function by integers numbers
+    static int Sum(int a, int b)
+    {
+        callCounter++;
+        return a + b;
+    }
+
+    //override the function by double
+    static double Sum(double a, double b)
+    {
+        callCounter++;
+        return a + b;
     }
 }
